a9296ec70e09bad1b45a3f87207d93755f585c60
Cong <xiaohanzi1001@gmail.com>
2017-05-08 22:09:15 +0200

    add add_to_cart button

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ root 'products#index'
 devise_for :users
 namespace :admin do
   resources :products
-end
-resources :products
+ end
+resources :products do
+  member do
+    post :add_to_cart
+  end
+ end
 end
